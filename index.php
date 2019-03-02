956a317776863c7e8e1e85eb12a2b50291920dff
kfang2 <47185626+kfang2@users.noreply.github.com>
2019-03-01 22:59:36 -0500

    pu

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="css/navigation.css">
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	<title> Team Hell </title>
+</head>
+
+	<body class="container">
+		<header>
+			<img src="images/hell.jpg" alt="hell">
+		</header>
+
+		<aside>
+			<ul>
+				<li>We have many mosquitos and extensive wetlands</li>
+				<li>We don't have a post office</li>
+				<li>We don't have our own local government</li>
+			</ul>
+		</aside>
+
+		<main>
+			<h2>City</h2>
+			<p>
+				Hell is an unincorporated community in Putnam Township, Livingston County, in the U.S. state of Michigan. The community is near the border with Washtenaw County, about 15 miles (24 km) northwest of Ann Arbor. Hell is three miles (4.8 km) southwest of Pinckney via Patterson Lake Road. The community is served by the Pinckney post office with ZIP Code 48169. <a href="https://en.wikipedia.org/wiki/Hell,_Michigan">Wikipedia</a>
+			</p>
+
+			<h2>Team</h2>
+			<p>
+				Out team is based in Hell, Michigan. Our team consists of Akshay, Sandesh, and Austin AKA Elon Musk. This site contains a brief overview of all of us!
+			</p>
+
+		</main>
+
+		<?php include "includes/footnav.inc"?>
